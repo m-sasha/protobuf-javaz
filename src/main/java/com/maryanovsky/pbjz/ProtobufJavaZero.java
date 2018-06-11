857199b6748dadd5d454f708df1e5c9f12999dfd
Alexander Maryanovsky <msasha@gmail.com>
2018-06-11 15:54:44 +0300

    Generating HelloWorld.java with JavaPoet.

--- src/main/java/com/maryanovsky/pbjz/ProtobufJavaZero.java
+++ src/main/java/com/maryanovsky/pbjz/ProtobufJavaZero.java
@@ -1,9 +1,16 @@
 package com.maryanovsky.pbjz;
 
 
+import com.squareup.javapoet.JavaFile;
+import com.squareup.javapoet.MethodSpec;
+import com.squareup.javapoet.TypeSpec;
+
 import static com.google.protobuf.compiler.PluginProtos.*;
 
 import java.io.IOException;
+
+import javax.lang.model.element.Modifier;
+
 
 public class ProtobufJavaZero{
 
@@ -11,12 +18,34 @@ public class ProtobufJavaZero{
 		CodeGeneratorRequest request = CodeGeneratorRequest.parseFrom(System.in);
 
 		CodeGeneratorResponse.Builder response = CodeGeneratorResponse.newBuilder();
+
+		
 		response.addFile(CodeGeneratorResponse.File.newBuilder()
 				.setName("HelloWorld.java")
-				.setContent("public class HelloWorld{\n\tpublic static void main(String[] args){\n\t\tSystem.out.println(\"Hello, World!\");\n\t}\n}")
+				.setContent(helloWorldClass())
 		);
 
 		response.build().writeTo(System.out);
+	}
+	
+	
+	private static String helloWorldClass(){
+		MethodSpec main = MethodSpec.methodBuilder("main")
+				.addModifiers(Modifier.PUBLIC, Modifier.STATIC)
+				.returns(void.class)
+				.addParameter(String[].class, "args")
+				.addStatement("$T.out.println($S)", System.class, "Hello, JavaPoet!")
+				.build();
+
+		TypeSpec helloWorld = TypeSpec.classBuilder("HelloWorld")
+				.addModifiers(Modifier.PUBLIC, Modifier.FINAL)
+				.addMethod(main)
+				.build();
+
+		JavaFile javaFile = JavaFile.builder("", helloWorld)
+				.build();
+
+		return javaFile.toString();
 	}
 
 }

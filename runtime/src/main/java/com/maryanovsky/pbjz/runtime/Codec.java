afa7e378519b057afcabf6633f44f4d18e0bf0ca
Alexander Maryanovsky <msasha@gmail.com>
2018-06-12 14:29:34 +0300

    Added methods for encoding protobuf primitives in Codec.

--- runtime/src/main/java/com/maryanovsky/pbjz/runtime/Codec.java
+++ runtime/src/main/java/com/maryanovsky/pbjz/runtime/Codec.java
@@ -31,4 +31,169 @@ public abstract class Codec<T>{
 
 
 
+	/**
+	 * Writes a {@code double} value at the given field number.
+	 */
+	protected final void writeDouble(CodedOutputStream out, int fieldNumber, double value) throws IOException{
+		if (value != 0D){
+			out.writeDouble(fieldNumber, value);
+		}
+	}
+
+
+
+	/**
+	 * Writes a {@code float} value at the given field number.
+	 */
+	protected final void writeFloat(CodedOutputStream out, int fieldNumber, float value) throws IOException{
+		if (value != 0F){
+			out.writeFloat(fieldNumber, value);
+		}
+	}
+
+
+
+	/**
+	 * Writes an {@code int} value at the given field number, encoded in {@code int32} format.
+	 */
+	protected final void writeInt32(CodedOutputStream out, int fieldNumber, int value) throws IOException{
+		if (value != 0){
+			out.writeInt32(fieldNumber, value);
+		}
+	}
+
+
+
+	/**
+	 * Writes a {@code long} value at the given field number, encoded in {@code int64} format.
+	 */
+	protected final void writeInt64(CodedOutputStream out, int fieldNumber, long value) throws IOException{
+		if (value != 0L) {
+			out.writeInt64(fieldNumber, value);
+		}
+	}
+
+
+
+	/**
+	 * Writes an {@code int} value at the given field number, encoded in {@code uint32} format.
+	 */
+	protected final void writeUInt32(CodedOutputStream out, int fieldNumber, int value) throws IOException{
+		if (value != 0){
+			out.writeUInt32(fieldNumber, value);
+		}
+	}
+
+
+
+	/**
+	 * Writes a {@code long} value at the given field number, encoded in {@code uint64} format.
+	 */
+	protected final void writeUInt64(CodedOutputStream out, int fieldNumber, long value) throws IOException{
+		if (value != 0L){
+			out.writeUInt64(fieldNumber, value);
+		}
+	}
+
+
+
+	/**
+	 * Writes an {@code int} value at the given field number, encoded in {@code sint32} format.
+	 */
+	protected final void writeSInt32(CodedOutputStream out, int fieldNumber, int value) throws IOException{
+		if (value != 0){
+			out.writeSInt32(fieldNumber, value);
+		}
+	}
+
+
+
+	/**
+	 * Writes a {@code long} value at the given field number, encoded in {@code sint64} format.
+	 */
+	protected final void writeSInt64(CodedOutputStream out, int fieldNumber, long value) throws IOException{
+		if (value != 0L){
+			out.writeSInt64(fieldNumber, value);
+		}
+	}
+
+
+
+	/**
+	 * Writes an {@code int} value at the given field number, encoded in {@code fixed32} format.
+	 */
+	protected final void writeFixed32(CodedOutputStream out, int fieldNumber, int value) throws IOException{
+		if (value != 0){
+			out.writeFixed32(fieldNumber, value);
+		}
+	}
+
+
+
+	/**
+	 * Writes a {@code long} value at the given field number, encoded in {@code fixed64} format.
+	 */
+	protected final void writeFixed64(CodedOutputStream out, int fieldNumber, long value) throws IOException{
+		if (value != 0L){
+			out.writeFixed64(fieldNumber, value);
+		}
+	}
+
+
+
+	/**
+	 * Writes an {@code int} value at the given field number, encoded in {@code sfixed32} format.
+	 */
+	protected final void writeSFixed32(CodedOutputStream out, int fieldNumber, int value) throws IOException{
+		if (value != 0){
+			out.writeSFixed32(fieldNumber, value);
+		}
+	}
+
+
+
+	/**
+	 * Writes a {@code long} value at the given field number, encoded in {@code sfixed64} format.
+	 */
+	protected final void writeSFixed64(CodedOutputStream out, int fieldNumber, long value) throws IOException{
+		if (value != 0L){
+			out.writeSFixed64(fieldNumber, value);
+		}
+	}
+
+
+
+	/**
+	 * Writes a {@code boolean} value at the given field number.
+	 */
+	protected final void writeBool(CodedOutputStream out, int fieldNumber, boolean value) throws IOException{
+		if (value){
+			out.writeBool(fieldNumber, true);
+		}
+	}
+
+
+
+	/**
+	 * Writes a {@link String} at the given field number.
+	 */
+	protected final void writeString(CodedOutputStream out, int fieldNumber, String value) throws IOException{
+		if (!value.isEmpty()){
+			out.writeString(fieldNumber, value);
+		}
+	}
+
+
+
+	/**
+	 * Writes a byte array at the given field number.
+	 */
+	protected final void writeBytes(CodedOutputStream out, int fieldNumber, byte[] value) throws IOException{
+		if (value.length != 0){
+			out.writeByteArray(fieldNumber, value);
+		}
+	}
+
+
+
 }

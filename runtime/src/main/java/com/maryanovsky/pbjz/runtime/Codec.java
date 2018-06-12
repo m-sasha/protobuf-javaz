c2ba40eff479e3838f3e443238c0574d4dd1cbc6
Alexander Maryanovsky <msasha@gmail.com>
2018-06-12 13:41:38 +0300

    Cleaned up Codec.java

--- runtime/src/main/java/com/maryanovsky/pbjz/runtime/Codec.java
+++ runtime/src/main/java/com/maryanovsky/pbjz/runtime/Codec.java
@@ -6,10 +6,29 @@ import com.google.protobuf.CodedOutputStream;
 
 import java.io.IOException;
 
+
+
+/**
+ * The base class for all types that encode and decode user-defined types into and from their
+ * protobuf wire representation.
+ * The class is parameterized with the user-defined type.
+ */
 public abstract class Codec<T>{
 
+
+
+	/**
+	 * Encodes the given value into a {@link CodedOutputStream}.
+	 */
 	public abstract void encode(T value, CodedOutputStream out) throws IOException;
 
+
+
+	/**
+	 * Decodes a single value from the given {@link CodedInputStream}.
+	 */
 	public abstract T decode(CodedInputStream in) throws IOException;
+
+
 
 }
